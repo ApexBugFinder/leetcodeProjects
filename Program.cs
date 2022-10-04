17cad2ca8300f28d968ea66cfc90ac0aa15ad6fe
Obasi Kong <apexbugfinder@gmail.com>
2022-10-04 10:27:53 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using Unitest;
+
+namespace LeetCodeTesting {
+  [TestFixture]
+  public class ValidSudokuTestCase {
+    [Test]
+    public void TestCase_1() {
+
+    }
+  }
+}
